11eddfc377ab00f351d9f02849165f92ae827ece
Thinaya <81453008+Thinaya@users.noreply.github.com>
2021-09-11 12:11:07 +1000

    Add files via upload

--- Kwitter.js
+++ Kwitter.js
@@ -1,6 +1,22 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAgE1N8QEgY8tkMZDuEyY2Kp896T2TKz4c",
+    authDomain: "kwitter2-4c9ea.firebaseapp.com",
+    databaseURL: "https://kwitter2-4c9ea-default-rtdb.firebaseio.com",
+    projectId: "kwitter2-4c9ea",
+    storageBucket: "kwitter2-4c9ea.appspot.com",
+    messagingSenderId: "506592828738",
+    appId: "1:506592828738:web:814c38713bfdb44f277cdb",
+    measurementId: "G-6FWRCD4MPB"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 function add_User()
 {
-    user_name = document.getElementById("user_name").value;
-    localStorage.setItem("user_name" , user_name);
-    window.location="kwitter_room.html";
+    user_name=document.getElementById("user_name").value;
+    firebase .database().ref("/").child(user_name).update({
+        purpose : "adding user"
+    });
+     window.location="kwitter_room.html";
 }
--- Kwitter_room.js
+++ Kwitter_room.js
@@ -1,10 +1,31 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAgE1N8QEgY8tkMZDuEyY2Kp896T2TKz4c",
+    authDomain: "kwitter2-4c9ea.firebaseapp.com",
+    databaseURL: "https://kwitter2-4c9ea-default-rtdb.firebaseio.com",
+    projectId: "kwitter2-4c9ea",
+    storageBucket: "kwitter2-4c9ea.appspot.com",
+    messagingSenderId: "506592828738",
+    appId: "1:506592828738:web:814c38713bfdb44f277cdb",
+    measurementId: "G-6FWRCD4MPB"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+function add_RoomName()
+{
+    roomName = document.getElementById("roomName").value;
+
+    firebase.database().ref("/").child(roomName).update({
+        purpose : "adding room name"
+    }) ;
+
+    localStorage.setItem("roomName", roomName);
+
+    window.location = "kwitter_page.html";
+}
+
 function log_out()
 {
       window.location="index.html";
-}
-
-function add_roomName()
-{
-  room_name = document.getElementById("room_name").value ;
-  localStorage("room_name" , room_name);
 }
